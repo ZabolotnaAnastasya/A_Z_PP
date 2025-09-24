b3d14f49244f423ee7bf0ea0d6a08c46db164ce2
A.Zabolotna <zabolotna.anastasya@gmail.com>
2025-09-25 00:55:40 +0300

    Оновлена версія програми: додано ArrayList та метод chek_w2_1

--- src/Fibo_num.java
+++ src/Fibo_num.java
@@ -1,16 +1,21 @@
 
 public class Fibo_num {
-    int n;
-    int val;
+    long n;
+    long val;
 
-    public Fibo_num(int n, int val) {    // конструктор
+    public Fibo_num(long n, long val) {    // конструктор
         this.n = n;
         this.val = val;
     }
 
+    public boolean chek_w2_1() {
+        int root = (int) Math.sqrt(this.val + 1);
+        return (root * root) == (this.val + 1);
+    }
+
     // гетери і сетери
-    public int getN() { return n; }
-    public int getVal() { return val; }
+    public long getN() { return n; }
+    public long getVal() { return val; }
 
     public void print() {
         System.out.println(this.n + " число по порядку, зі значенням " + this.val);
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -13,27 +14,41 @@ public class Main {
             n = ans.nextInt();               //для вводу
         }
 
-        checkSquares(n);
+        //динамічний лист
+        ArrayList<Fibo_num> specialNumbers = generate_fibo(n);
+
+        System.out.println("\nЧисла Фібоначчі, що дорівнюють w*w - 1:");
+        if (specialNumbers.isEmpty()) {
+            System.out.println("На цьому проміжку нема таких чисел.");
+        } else {
+            print_list(specialNumbers);
+        }
+
     }
 
-    static void checkSquares(int n) {
-        int rule = 1;
-        int a = 1, b = 1, c;
+    static ArrayList<Fibo_num> generate_fibo(int n) {
+        ArrayList<Fibo_num> result = new ArrayList<>();
+        long a = 1,b = 1, c; //перші 2 числа після віднімання 1 дають 0, що не є квадратом числа фібоначі
+        int num = 3;
 
-        for (int i = 0; i < n; ) {
+        n +=3; //бо лік іде від числа а, а задаєм число фібоначі числоб с = а + 3
+        for (int i = 3; i <= n; i++) {
             c = a + b;
-            if (c == rule * rule - 1) {
-                Fibo_num first_fibo = new Fibo_num(i + 3, c);
-                first_fibo.print();
-            }
-            if (c > rule * rule - 1) {
-                rule++;
-                continue;
+            Fibo_num fibo = new Fibo_num(i, c);
+            if (fibo.chek_w2_1()){
+                result.add(fibo);
             }
             a = b;
             b = c;
-            i++;
         }
+        return result;
     }
 
+
+
+    static void print_list(ArrayList<Fibo_num> arr) {
+        for (Fibo_num fibo : arr) {
+            fibo.print();
+        }
+    }
 }
